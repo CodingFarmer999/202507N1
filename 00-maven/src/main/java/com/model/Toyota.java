f6da49f782c2d77e219fc67e99aa1be0495c5616
CodingFarmer999 <codingfarmer999@gmail.com>
2025-07-23 19:38:24 +0800

    Car

--- 00-maven/src/main/java/com/model/Toyota.java
+++ 00-maven/src/main/java/com/model/Toyota.java
@@ -2,11 +2,16 @@ package com.model;
 
 public class Toyota /** extends ToyotaEngine */ {
 
+	private ToyotaEngine engine;
+	
+	public Toyota(ToyotaEngine engine) {
+		this.engine = engine;
+	}
 	
 	public void move() {
 		// ToyotaEngine engine = new ToyotaEngine();
 		// engine.start();
-		// start();
+		engine.start();
 		System.out.println("Toyota 移動");
 	}
 }
