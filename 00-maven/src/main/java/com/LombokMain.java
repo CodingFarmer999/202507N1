503f8abe304d159162bac64a7c781886bc58ff44
CodingFarmer999 <codingfarmer999@gmail.com>
2025-07-23 18:56:15 +0800

    Lombok

--- 00-maven/src/main/java/com/LombokMain.java
+++ 00-maven/src/main/java/com/LombokMain.java
@@ -5,9 +5,10 @@ import com.model.User;
 public class LombokMain {
 
 	public static void main(String[] args) {
-		User user = new User();
-		
-		user.getName();
+		User user1 = new User();
+		user1.setName("AAAA");
+		User user2 = new User(1L, "AAAA");
+		System.out.println(user1.equals(user2));
 	}
 
 }
