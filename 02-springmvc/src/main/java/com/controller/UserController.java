76b5a982f6cd026f01102fa896e5a340694d869d
CodingFarmer999 <codingfarmer999@gmail.com>
2025-07-28 20:28:12 +0800

    login

---
+++ 02-springmvc/src/main/java/com/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// @Component
+@Controller
+public class UserController {
+
+	@RequestMapping("/login")
+	public String login() {
+		// "/WEB-INF/views/login.jsp" 
+		return "login";
+	}
+}
