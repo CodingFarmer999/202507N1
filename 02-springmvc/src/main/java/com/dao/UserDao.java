2b17e46736cc5b4404975201f17062a6dcac206c
CodingFarmer999 <codingfarmer999@gmail.com>
2025-07-30 19:36:38 +0800

    UserDaoImpl

--- 02-springmvc/src/main/java/com/dao/UserDao.java
+++ 02-springmvc/src/main/java/com/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.dao;
 
+import com.entity.UserEntity;
+
 public interface UserDao {
 
+	public UserEntity findByUsernameAndPassword(String username, String password);
 }
--- 02-springmvc/src/main/java/com/dao/impl/UserDaoImpl.java
+++ 02-springmvc/src/main/java/com/dao/impl/UserDaoImpl.java
@@ -1,10 +1,25 @@
 package com.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import com.dao.UserDao;
+import com.entity.UserEntity;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+
+	@Override
+	public UserEntity findByUsernameAndPassword(String username, String password) {
+		// 連線取得 DB 資料
+		
+	    Configuration configuration = new Configuration().configure();
+	    SessionFactory sessionFactory = configuration.buildSessionFactory();
+	    Session session = sessionFactory.openSession();
+	    
+		return null;
+	}
 
 }
