2bb6678d1ed7bbd72d0a184745b24c641482742f
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-04 20:58:52 +0800

    Hello Service

--- 03-hello-springboot/src/main/java/com/course/Application.java
+++ 03-hello-springboot/src/main/java/com/course/Application.java
@@ -2,12 +2,16 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		for (String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
