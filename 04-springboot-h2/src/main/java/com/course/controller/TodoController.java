ca766c78a5148440f8c03564a81b507660529928
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-06 21:22:27 +0800

    add

--- 04-springboot-h2/src/main/java/com/course/controller/TodoController.java
+++ 04-springboot-h2/src/main/java/com/course/controller/TodoController.java
@@ -1,9 +1,17 @@
 package com.course.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.course.service.TodoService;
+
+@Controller
 public class TodoController {
 
+	@Autowired
+	private TodoService todoService;
+	
 	@GetMapping("/add")
 	public String addTodo() {
 		todoService.addTodo();
