819f6f26d8c8e316e93a68e46dcdb8167feff93b
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-06 20:57:03 +0800

    DaoImpl

--- 04-springboot-h2/src/main/java/com/course/dao/impl/TodoDaoImpl.java
+++ 04-springboot-h2/src/main/java/com/course/dao/impl/TodoDaoImpl.java
@@ -1,5 +1,40 @@
 package com.course.dao.impl;
 
-public class TodoDaoImpl {
+import java.util.List;
+
+import com.course.dao.TodoDao;
+import com.course.model.TodoDto;
+
+public class TodoDaoImpl implements TodoDao {
+
+	@Override
+	public void add(TodoDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(TodoDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<TodoDto> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TodoDto> findByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
