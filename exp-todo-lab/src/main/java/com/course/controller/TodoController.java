3437577051b71a6e2e77f14194c6410f0198bf00
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-08 20:24:52 +0800

    TodoController

--- exp-todo-lab/src/main/java/com/course/controller/TodoController.java
+++ exp-todo-lab/src/main/java/com/course/controller/TodoController.java
@@ -1,5 +1,13 @@
 package com.course.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TodoController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
