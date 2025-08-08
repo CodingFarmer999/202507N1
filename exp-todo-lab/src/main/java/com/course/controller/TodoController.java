9df9c55ec8e114f71654b3212e7804d0fb968eaf
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-08 20:43:12 +0800

    TODO

--- exp-todo-lab/src/main/java/com/course/controller/TodoController.java
+++ exp-todo-lab/src/main/java/com/course/controller/TodoController.java
@@ -1,13 +1,26 @@
 package com.course.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.course.model.TodoDto;
+import com.course.service.TodoService;
 
 @Controller
 public class TodoController {
 
+	@Autowired
+	private TodoService todoService;
+	
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+    	List<TodoDto> todoList = todoService.findAllTodo();
+    	model.addAttribute("todoList", todoList);
         return "index";
     }
+    
 }
--- exp-todo-lab/src/main/java/com/course/service/TodoService.java
+++ exp-todo-lab/src/main/java/com/course/service/TodoService.java
@@ -1,5 +1,24 @@
 package com.course.service;
 
-public class TodoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.dao.TodoDao;
+import com.course.model.TodoDto;
+
+@Service
+public class TodoService {
+	
+	@Autowired
+	private TodoDao todoDao;
+	
+	public List<TodoDto> findAllTodo() {
+		List<TodoDto> dtoList = todoDao.findAll();
+		for (TodoDto dto : dtoList) {
+			System.out.println(dto);
+		}
+		return dtoList;
+	}
 }
