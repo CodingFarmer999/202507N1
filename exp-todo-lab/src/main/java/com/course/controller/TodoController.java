3c83084820ef959aa68af0a1d2b45bbd6ed4b532
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-11 19:08:11 +0800

    utext

--- exp-todo-lab/src/main/java/com/course/controller/TodoController.java
+++ exp-todo-lab/src/main/java/com/course/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.course.model.TodoVo;
 import com.course.service.TodoService;
@@ -21,6 +22,12 @@ public class TodoController {
     	List<TodoVo> todoList = todoService.findAllTodo();
     	model.addAttribute("todoList", todoList);
         return "index";
+    }
+    
+    @ModelAttribute("title")
+    public String title() {
+    	return "待辦事項222";
+//    	return "<script>alert('!!!!!!')</script>";
     }
     
 }
