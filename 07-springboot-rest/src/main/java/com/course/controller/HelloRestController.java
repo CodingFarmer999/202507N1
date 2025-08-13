54b5ca1615dd5466cabd44f3f15cbab93ba6c772
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-13 21:26:49 +0800

    hello restful

---
+++ 07-springboot-rest/src/main/java/com/course/controller/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.course.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloRestController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello";
+	}
+}
