234e1c5b1eeb7997df54c47bc3485797ab9f5e38
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-15 19:12:58 +0800

    GetMapping

--- 07-springboot-rest/src/main/java/com/course/controller/HelloRestController.java
+++ 07-springboot-rest/src/main/java/com/course/controller/HelloRestController.java
@@ -1,12 +1,17 @@
 package com.course.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.model.User;
+
 //@Controller
 @RestController
-@RequestMapping(value = "/kitty")
+//@RequestMapping(value = "/kitty")
 public class HelloRestController {
 
 	@GetMapping("/hello")
@@ -19,5 +24,12 @@ public class HelloRestController {
 //	@ResponseBody
 	public String hello2() {
 		return "hello2";
+	}
+	
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		User user1 = new User("Kitty", "1234");
+		User user2 = new User("Snoopy", "1234");
+		return Arrays.asList(user1, user2);
 	}
 }
