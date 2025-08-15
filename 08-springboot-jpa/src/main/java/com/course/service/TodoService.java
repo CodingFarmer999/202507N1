9ee495f72e4c445c90931ea4535465bc2ac0ee26
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-15 21:37:24 +0800

    Controller

--- 08-springboot-jpa/src/main/java/com/course/service/TodoService.java
+++ 08-springboot-jpa/src/main/java/com/course/service/TodoService.java
@@ -1,5 +1,19 @@
 package com.course.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.course.entity.TodoEntity;
+
+@Service
 public class TodoService {
 
+	/**
+	 * 取得所有待辦事項
+	 * @return
+	 */
+	public List<TodoEntity> getAllTodo() {
+		return null;
+	}
 }
