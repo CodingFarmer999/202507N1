457ff99b11b22a3408792966a1924d727423582a
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-15 19:54:35 +0800

    Swagger

--- 07-springboot-rest/src/main/java/com/course/controller/HelloRestController.java
+++ 07-springboot-rest/src/main/java/com/course/controller/HelloRestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.course.model.User;
@@ -25,7 +27,8 @@ public class HelloRestController {
 		return "hello kitty";
 	}
 	
-	@GetMapping("/hello2")
+	// GetMapping
+	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
 //	@ResponseBody
 	public String hello2() {
 		return "hello2";
