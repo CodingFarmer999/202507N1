75d8a7f32d818b8e23f41e6012230564ccce2a03
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-22 21:19:29 +0800

    Category

--- exp-ec-lab/src/main/java/com/course/entity/CategoryEntity.java
+++ exp-ec-lab/src/main/java/com/course/entity/CategoryEntity.java
@@ -1,5 +1,22 @@
 package com.course.entity;
 
-public class CategoryEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "CATEGORY")
+public class CategoryEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CATEGORY_SEQ_GENERATOR")
+	@SequenceGenerator(name = "CATEGORY_SEQ_GENERATOR", sequenceName = "CATEGORY_SEQ", allocationSize = 1)
+	private Long id;
+	
+	@Column
+	private String name;
 }
