1f96e2c2c268cd88f932a047fc6ef9561e4b3f25
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-22 20:33:16 +0800

    Review Entity

--- exp-ec-lab/src/main/java/com/course/entity/ProductReviewEntity.java
+++ exp-ec-lab/src/main/java/com/course/entity/ProductReviewEntity.java
@@ -1,5 +1,26 @@
 package com.course.entity;
 
-public class ProductReviewEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "PRODUCT_REVIEW")
+public class ProductReviewEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUCT_REVIEW_SEQ_GENERATOR")
+	@SequenceGenerator(name = "PRODUCT_REVIEW_SEQ_GENERATOR", sequenceName = "PRODUCT_REVIEW_SEQ", allocationSize = 1)
+	private Long id;
+	
+	@Column
+	private String memo;
+	
+	@Column
+	private Long productId;
 }
