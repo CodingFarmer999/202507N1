559972bc34fc2fff30bb4903829f8eda820bbdb9
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-27 21:27:51 +0800

    @Before

--- exp-ec-lab/src/main/java/com/course/aspect/EcAspect.java
+++ exp-ec-lab/src/main/java/com/course/aspect/EcAspect.java
@@ -1,10 +1,27 @@
 package com.course.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class EcAspect {
 
+	Logger logger = LoggerFactory.getLogger(EcAspect.class);
+	
+	// execution(scope return-type fully-qualified-class-name .*(parameters))
+	@Pointcut("execution(public * com.course.service.ProductService.*(..))")
+	public void pointCutMethod() {
+	    // 透由@Pointcut定義切點，方法內容維持空方法
+	}
+	
+	@Before("pointCutMethod()")
+	public void beforeAdvice(JoinPoint joinPoint) {
+		logger.info("@Before: " + joinPoint.getSignature().getName());
+	}
 }
