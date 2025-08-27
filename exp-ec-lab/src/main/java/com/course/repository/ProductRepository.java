1a681fd1830846ed8453119727a86d3acda6522a
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-27 18:56:07 +0800

    Query

--- exp-ec-lab/src/main/java/com/course/repository/ProductRepository.java
+++ exp-ec-lab/src/main/java/com/course/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.course.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.course.entity.ProductEntity;
@@ -8,4 +11,6 @@ import com.course.entity.ProductEntity;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
 
+	@Query("select p.code, p.name from ProductEntity p")
+	public List<ProductEntity> findByQuery();
 }
