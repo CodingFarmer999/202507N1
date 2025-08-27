4b7593cdd741190c7e2f897e4d1a74cd3064e3b8
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-27 19:05:16 +0800

    Query

--- exp-ec-lab/src/main/java/com/course/repository/ProductRepository.java
+++ exp-ec-lab/src/main/java/com/course/repository/ProductRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.course.dto.ProductDto;
 import com.course.entity.ProductEntity;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
 
-	@Query("select p.code, p.name from ProductEntity p")
-	public List<ProductEntity> findByQuery();
+	@Query("select new com.course.dto.ProductDto(p.code, p.name) from ProductEntity p")
+	public List<ProductDto> findByQuery();
+	
+//	@Query("select new com.course.dto.ProductDto(p.code, p.name, r.listPrice) from ProductEntity p join ProductPriceEntity r on r.productId = p.id")
+//	public List<ProductDto> findByQuery2();
 }
