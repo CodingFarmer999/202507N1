3cde870d773307eac09f5fba1a65a05b3cdb9462
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-27 19:39:06 +0800

    handle Action Exception

--- exp-ec-lab/src/main/java/com/course/advice/GlobalExceptionHandler.java
+++ exp-ec-lab/src/main/java/com/course/advice/GlobalExceptionHandler.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.course.exception.ActionException;
 import com.course.model.ApiResponse;
 
 @RestControllerAdvice
@@ -42,6 +42,14 @@ public class GlobalExceptionHandler {
 		
         // 回傳自訂格式的錯誤回應（HTTP 200，responseCode 代表錯誤）
         ApiResponse<Map<String, String>> response = ApiResponse.error("505", "系統忙碌中", null);
+
+        return response;
+	}
+	
+	@ExceptionHandler(value = ActionException.class)
+	public ApiResponse<Map<String, String>> actionExceptionHandler(ActionException ae) {
+		logger.error("Action error", ae);
+        ApiResponse<Map<String, String>> response = ApiResponse.error("999", "系統忙碌中，請稍後再試", null);
 
         return response;
 	}
