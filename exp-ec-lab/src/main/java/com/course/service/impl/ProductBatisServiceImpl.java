4e86c90d59c1374b7c3911f3ed7827f7178156ed
CodingFarmer999 <codingfarmer999@gmail.com>
2025-08-29 19:48:29 +0800

    Extract interface

---
+++ exp-ec-lab/src/main/java/com/course/service/impl/ProductBatisServiceImpl.java
@@ -0,0 +1,67 @@
+package com.course.service.impl;
+
+import java.util.List;
+
+import com.course.dto.ProductDto;
+import com.course.entity.ProductEntity;
+import com.course.service.ProductService;
+import com.course.vo.ProductQueryParam;
+import com.course.vo.ProductVo;
+
+public class ProductBatisServiceImpl implements ProductService {
+
+	@Override
+	public void addProductForEach(ProductVo vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addProduct(ProductVo vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductEntity> getAllProductReturnEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductVo> getAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductVo> getAllProductForPrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductVo getProductById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductDto> getAllProductData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductDto> getProductByCondition(ProductQueryParam queryParam) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductVo> getAllProductQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
